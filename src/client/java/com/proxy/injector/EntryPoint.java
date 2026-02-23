63b32736e4a98679e10b843c375a1ec907cea21f
Justin Angara <justin.angara@gmail.com>
2026-02-23 11:20:13 -0600

    setup even more

---
+++ src/client/java/com/proxy/injector/EntryPoint.java
@@ -0,0 +1,12 @@
+package com.proxy.injector;
+
+public class EntryPoint {
+    public static void setup(){
+        // get the jni to add to gradle
+
+        // call the c++ entry
+
+        // bang proxy injection occured
+
+    }
+}
