376217c2ea3eb79ea7bb3d38fbd980658349a985
Justin Angara <justin.angara@gmail.com>
2026-02-24 19:50:43 -0600

    check

--- src/client/java/com/proxy/injector/EntryPoint.java
+++ src/client/java/com/proxy/injector/EntryPoint.java
@@ -1,12 +1,18 @@
 package com.proxy.injector;
 
 public class EntryPoint {
+    private EntryPoint(){}
+    static {
+        System.loadLibrary("nativebridge");
+    }
     public static void setup(){
         // get the jni to add to gradle
 
         // call the c++ entry
 
         // bang proxy injection occured
-
+        call();
     }
+    private static native void call();
+
 }
